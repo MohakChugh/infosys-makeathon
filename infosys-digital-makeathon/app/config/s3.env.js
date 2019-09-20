98e7bbb61dc771696bfc18e4372adc8922514015
Mohak Chugh <me.mohakchugh@gmail.com>
2019-09-20 20:03:09 +0530

    key removed

--- infosys-digital-makeathon/app/config/s3.env.js
+++ infosys-digital-makeathon/app/config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-  AWS_ACCESS_KEY: "AKIAZW6MW2UZXFB2GS4K",
-  AWS_SECRET_ACCESS_KEY: "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",
+  AWS_ACCESS_KEY: "",
+  AWS_SECRET_ACCESS_KEY: "",
   REGION: "us-east-2",
   Bucket: "problem-image-upload"
 };
